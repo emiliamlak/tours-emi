c31bf2b99e31e8d0f801fb81530c95e3a64e55a2
EmiliaMlak <emiliamlak@gmail.com>
2024-01-10 14:23:13 +0000

    remove tour

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [tours, setTours] = useState([]);
 
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setIsLoading(true)
     try {
@@ -33,6 +39,6 @@ const App = () => {
     )
   }
 
-  return <main><Tours tours={tours} /></main>;
+  return <main><Tours tours={tours} removeTour={removeTour} /></main>;
 };
 export default App;
